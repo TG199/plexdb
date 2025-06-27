19822372330e1b81c2795c915b171637bbfb95e2
Kelechi Ebiri <ebiritg@gmail.com>
2025-06-27 15:13:38 +0100

    Error handling - Use std error
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,53 @@
-use std::io;
+use std::{io, fmt};
 use thiserror::Error;
+use bincode;
 
 
 #[derive(Error, Debug)]
 pub enum KvError {
-    #[error("Key not found")]
+    /// The key was not found in the store
     KeyNotFound,
 
-    #[error("I/O error: {0}")]
-    Io(#[from] io::Error),
+    /// The key provided was empty.
+    KeyIsEmpty,
 
-    #[error("Serialization error: {0}")]
-    Serde(#[from] bincode::Error),
+    /// An I/O error occurred
+    IO(io::Error),
 
+    /// A deseerialization error occurred while loading a command.
+    Deserialize(bincode::Error),
 
-    #[error("Unexpected error: {0}")]
-    Unexpected(String),
+    /// Corrupt ot invalid data was found at a specific file offset.
+    CorruptData(u64),
+
+}
+
+impl fmt::Display for KvError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        match self {
+            KvError::KeyIsEmpty => write!(f, "The key is empty"),
+            KvError::KeyNotFound => write!(f, "Key not found"),
+            KvError::IO(err) => write!(f, "I/O error: {}", err),
+            KvError::Deserialize(err) => write!(f, "Deserialization error: {}", err),
+            KvError::CorruptData(offset) => {
+                write!(f, "Corrupt data detected at file offset {}", offset)
+            }
+        }
+    }
+}
+
+impl std::error::Error for KvError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            KvError::IO(err) => Some(err),
+            KvError::Deserialize(err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+impl From<bincode::Error> for KvError {
+    fn from(err: bincode ::Error) -> KvError {
+        KvError::Deserialize(err)
+    }
 }
