570af70831eddd71d87b0e2e5f2c3caf9ae9759b
Kelechi Ebiri <ebiritg@gmail.com>
2025-07-01 23:11:21 +0100

    Fix - main file errors
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use kaydb::KvError;
+use kaydb::StorageEngine;
 use kaydb::engine::file_storage::FileEngine;
 use kaydb::cli::{CliArgs, Command};
 use clap::Parser;
+use anyhow::bail;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let args = cli::CliArgs::parse();
-    let path = args.data_dir..join("data/data.log");
+    let args = CliArgs::parse();
+    let path = args.data_dir.join("data/data.log");
     let mut store = FileEngine::new(path)?;
 
     match args.command {
@@ -19,7 +21,7 @@ fn main() -> anyhow::Result<()> {
         Command::Get { key } => {
             match store.get(&key)? {
                 Some(val) => println!("{}", val),
-                None => KvError::KeyNotFound(key),
+                None => bail!(KvError::KeyNotFound),
             }
         }
 
