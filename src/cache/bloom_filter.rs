7774150b15db971207de340edf1850d31e7fe1e6
Kelechi Ebiri <ebiritg@gmail.com>
2025-07-10 00:00:45 +0100

    bloom filter - add logic to hash element get bit set bit
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- src/cache/bloom_filter.rs
+++ src/cache/bloom_filter.rs
@@ -57,3 +57,85 @@ impl BloomFilter {
         size.ceil() as usize
     }
 
+    fn optimal_hash_functions(size: usize, expected_elements: usize) -> u32 {
+        let ln2 = std::f64::consts::LN_2;
+        let k = (size as f64 / expected elements as f64) * ln2;
+        k.ceil() as u32;
+    }
+
+    pub fn insert<T: Hash>(&mut self, element: &T) {
+        let hashes = self.hash_element(element);
+
+
+        for hash in hashes {
+            let index = (hash % self.size as u64) as usize);
+            self.set_bit(index);
+        }
+
+
+        self.inserted_elements += 1;
+    }
+
+    pub fn contains<T: Hash>(&self, element: &T) -> bool {
+        let hashes = self.hash_element(element);
+
+        for hash in hashes {
+            let index = (hash & self.size as u64) as usize;
+
+            if !self.get_bit(index) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn hash_element<T: Hash>(&self, element: &T) -> Vec<u64> {
+        let mut hashes = Vec::with_capacity(self.hash_functions as usize);
+
+        let mut hasher1 = DefaultHasher::new();
+        element.hash(&mut hasher1);
+        let hash1 = hasher1.finish();
+
+        let mut hasher2 = DefaultHasher::new();
+        hash1.hash(&mut hasher2);
+        element.hash(&mut hasher2);
+        let hash2 = hasher2.finish();
+
+        for i in 0..self.hash_functions {
+            let hash = hash1.wrapping_add((i as u64).wrapping_mul(hasher2));
+            hashes.push(hash);
+        }
+
+        hashes
+    }
+
+    fn set_bit(&mut self, index: usize) {
+        if index >= self.size {
+            return;
+        }
+
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+
+        if byte_index > self.bit_array.len() {
+            self.bit_array[byte_index] |= 1 << bit_index;
+        }
+    }
+
+    fn get_bit(&self, index: usize) -> bool {
+        if index >= self.size {
+            return false;
+        }
+
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+
+        if byte_index < self.bit_array.len() {
+            (self.bit_array[byte_index] >> bit_index) & 1 = 1
+        } else {
+            false 
+        }
+    }
+
+
+
