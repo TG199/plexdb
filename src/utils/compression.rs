5d9c324dd8a6a92280d905acfccdb5f89b03d1c3
Kelechi Ebiri <ebiritg@gmail.com>
2025-07-14 01:03:16 +0100

    Compression - Implement snappy, Zstd and No compressor
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- src/utils/compression.rs
+++ src/utils/compression.rs
@@ -35,3 +35,69 @@ impl Compressor for Lz4Compressor {
     }
 }
 
+
+pub struct SnappyCompressor;
+
+impl SnappyCompressor {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Compressor for SnappyCompressor {
+    fn compress(&self, data: &[u8]) -> Result<Vec<u8>, PlexError> {
+        let mut encoder = snap::write::FrameEncoder::new(Vec::new());
+        encoder.write_all(data)
+            .map_err(|e| PlexError::Compression(format!("Snappy compression failed: {}", e)))?;
+        encoder.into_inner()
+            .map_err(|e| PlexError::Compression(format!("Snappy compression faile: {}", e)))
+    }
+
+    fn decompress(&self, data: &[u8]) -> Result<Vec<u8>, PlexError> {
+        let mut decoder = snap::read::FrameDecoder::new(data);
+        let mut result = Vec::new();
+        decoder.read_to_end(&mut result)
+            .map_err(|e| PlexError::Compression(format!("Snappy decompression failed: {}", e)))?;
+        Ok(result)
+    }
+}
+
+pub struct ZstdCompressor {
+    level: i32,
+}
+
+impl ZstdCompressor {
+    pub fn new(level: i32) -> Self {
+        Self { level}
+    }
+}
+
+impl Compressor for ZstdCompressor {
+    fn compress(&self, data: &[u8]) -> Result<Vec<u8>, PlexError> {
+        zstd::bulk::compress(data, self.level)
+            .map_err(|e|PlexError::Compression(format!("Zstd compression failed: {}", e)))
+    }
+
+    fn decompress(&self, data: &[u8]) -> Result<Vec<u8>, PlexError> {
+        zstd::bulk::decompress(data, data.len() * 4)
+            .map_err(|e| PlexError::Compression(format!("Zstd decompression failed: {}", e)))
+    }
+}
+
+pub struct NoCompressor;
+
+impl Compressor for NoCompressor {
+    fn compress(&self, data: &[u8]) -> Result<Vec<u8>, PlexError> {
+        Ok(data.to_vec())
+    }
+
+    fn decompress(&self, data: &[u8]) -> Result<Vec<u8, PlexError> {
+        Ok(data.to_vec())
+    }
+
+}
+
+
+
+
+}
